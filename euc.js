118ea85589561e0167d0d605c4a786712fcc016b
A. M. Douglas <amd@xmli5.io>
2016-06-05 06:11:43 +0100

    refactor API

--- euc.js
+++ euc.js
@@ -1,61 +1,53 @@
 ;(function(){
-
   "use strict";
 
-  function eucAlert(config){
-
-    var cookieName   = config.cookieName   ? config.cookieName      : "euc",
-        cookieFlag   = config.cookieFlag   ? config.cookieFlag      : "true",
-        cookieMaxAge = config.cookieMaxAge ? config.cookieMaxAge    : 14,
-        alertId      = config.alertId      ? config.alertId         : "cookieAlert",
-        alertContent = config.alertContent ? config.alertContent    : "",
-        alertHook    = config.alertHook    ? config.alertHook       : "has-cookie-alert",
-        noCookie     = document.cookie.indexOf(cookieName) < 0 ? !0 : !!0;
+    var settings;
 
     var euc = {
 
-      showAlert : function (){
+      init: function(config){
+        settings = {
+          cookieName   : config.cookieName   ? config.cookieName   : "euc",
+          cookieFlag   : config.cookieFlag   ? config.cookieFlag   : "true",
+          cookieMaxAge : config.cookieMaxAge ? config.cookieMaxAge : 14,
+          alertId      : config.alertId      ? config.alertId      : "cookieAlert",
+          alertContent : config.alertContent ? config.alertContent : "",
+          alertHook    : config.alertHook    ? config.alertHook    : "show-cookie-alert"
+        };
 
+        if (document.cookie.indexOf(settings.cookieName) < 0)
+        {
+          euc.showAlert();
+        }
+
+      },
+
+      showAlert : function(){
         var bodyElement        = document.getElementsByTagName("body")[0],
             alertElement       = document.createElement("div");
-
-        alertElement.id        = alertId,
-        alertElement.innerHTML = alertContent,
-
-        bodyElement.className  += " " + alertHook,
+        alertElement.id        = settings.alertId,
+        alertElement.innerHTML = settings.alertContent,
+        bodyElement.className  += " " + settings.alertHook,
         bodyElement.appendChild(alertElement);
-
       },
 
       setCookie : function(){
-
         var d = new Date();
-
-        d.setTime(d.getTime() + 1e3 * 60 * 60 * 24 * cookieMaxAge);
-
-        document.cookie = 
-                      (
-                        cookieName
+        d.setTime(d.getTime() + 1e3 * 60 * 60 * 24 * settings.cookieMaxAge);
+        document.cookie = (settings.cookieName
                         + "="
-                        + cookieFlag
-                        + (cookieMaxAge ? "; max-age=" + d.toGMTString() : "")
-                        + "; path=/"
-                      );
+                        + settings.cookieFlag
+                        + (settings.cookieMaxAge ? "; max-age=" + d.toGMTString() : "")
+                        + "; path=/");
       },
 
       removeAlert : function(){
-        document.getElementById(alertId).parentNode.removeChild(document.getElementById(alertId));
+        document.getElementById(settings.alertId).parentNode.removeChild(document.getElementById(settings.alertId));
         euc.setCookie();
-      }
+      },
 
-    };
+    }
 
     window.euc = euc;
-
-    !!noCookie && euc.showAlert();
-
-  }
-
-  window.eucInit = eucInit;
 
 }());
